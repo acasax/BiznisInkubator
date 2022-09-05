6491200bdb7c4a17fe12270c8ae328721afebbec
acasax <acasax@gmail.com>
2022-09-05 13:59:05 +0200

    add new image to gallery

--- admin/connection.php
+++ admin/connection.php
@@ -1,8 +1,8 @@
 <?php
     $db_host="localhost";
-    $db_user="root";
-    $db_password="";
-    $db_name="bic";
+    $db_user="biznisin_blog";
+    $db_password="blog123!$$";
+    $db_name="biznisin_bic";
 
     try{
         $db= new PDO("mysql:host={$db_host};dbname={$db_name}",$db_user,$db_password);
